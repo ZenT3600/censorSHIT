7ab28d918b8308cf2d162305357cc12d50b57c64
ZenT3600 <matteo.leggio@tiscali.it>
2023-08-23 17:37:19 +0200

    Introduce CLI parameters

--- censorSHIT.sh
+++ censorSHIT.sh
@@ -2,11 +2,40 @@
 
 set -e
 
+function show_help() {
+    echo "
+ censorSHIT - Simple script to avoid hash-based recognition systems on messaging apps
+ ===
+ -v | --verbose
+    Enable debug-level logs
+
+ -h | --help
+    Show this help message and quit
+
+ -l | --logger
+    Specify the method to log message to the console
+    Options are: echo, gum, quiet
+
+ [ POSITIONAL ]
+    Specify the video to modify
+"
+}
+
 function log_echo() {
-    echo "[$2] $1"
+    if [ ! $VERBOSE ]; then
+        if [ $2 == $C_DEBUG ]; then
+            return
+        fi
+    fi
+    echo "($2) [$(date)] $1"
 }
 
 function log_gum() {
+    if [ ! $VERBOSE ]; then
+        if [ $2 == $C_DEBUG ]; then
+            return
+        fi
+    fi
     echo '{{Color "'$2'" " (*)"}}{{ Color "'$C_MUTE'" " ['$(date)']" }}' $1 | gum format -t template
 }
 
@@ -53,41 +82,101 @@ function random_area() {
 }
 
 
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -v|--verbose)
+        VERBOSE=true
+        shift
+        ;;
+    -h|--help)
+        show_help
+        exit 0
+        ;;
+    -l|--logger)
+        LOGGER=$2
+        case $LOGGER in
+            "gum")
+                LOG="log_gum"
+                ;;
+            "echo")
+                LOG="log_echo"
+                ;;
+            "quiet")
+                LOG=":"
+                ;;
+            *)
+                $LOG "Invalid logger: $LOGGER" $C_ERR
+                exit 1
+                ;;
+        esac
+        shift
+        shift
+        ;;
+    -*|--*)
+        $LOG "Invalid option: $i" $C_ERR
+        exit 1
+        ;;
+    *)
+        POSITIONAL_ARGS+=("$1")
+        shift
+        ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}"
+
 INVID="$1"
 if [ -z $INVID ]; then
     $LOG "Invalid input file" $C_ERR
     exit -1
 fi
-TMP="tmp/"
+TMP="tmp.$(openssl rand -base64 6)/"
+$LOG "VERBOSE = $VERBOSE" $C_DEBUG
+$LOG "TMP = $TMP" $C_DEBUG
+$LOG "LOGGER = $LOGGER" $C_DEBUG
+$LOG "VIDEO = $INVID" $C_DEBUG
 
 $LOG "Creating temporary directory at $TMP ..." $C_INFO
 mkdir -p $TMP
+
 FPS=$(ffprobe -v 0 -of compact=p=0 -select_streams 0 -show_entries stream=r_frame_rate $INVID | sed 's/r_frame_rate=//g')
-$LOG "Found FPS to be $FPS!" $C_DEBUG
+$LOG "FPS = $FPS" $C_DEBUG
 SIZE=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 $INVID)
-$LOG "Found SIZE to be $SIZE!" $C_DEBUG
+$LOG "SIZE = $SIZE" $C_DEBUG
+
 $LOG "Exporting orignal video frames into temporary directory ..." $C_INFO
 $FFMPEG -i $INVID -r $FPS $TMP$filename%03d.jpg
+
 $LOG "Modifying original video frames  ..." $C_INFO
 find $TMP -type f | while read INFRAME; do
     mogrify -fill "$(random_hex)" -draw " rectangle $(random_area $SIZE) " $INFRAME
 done
+
 $LOG "Exporting orignal audio into temporary directory ..." $C_INFO
 $FFMPEG -i $INVID -vn -acodec libmp3lame ${TMP}input-audio.mp3
-NOISE="noise.mp3"
+
 LENGTH=$(ceil $(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $INVID) )
-$LOG "Found LENGTH to be $LENGTH!" $C_DEBUG
+$LOG "LENGTH = $LENGTH" $C_DEBUG
+
 $LOG "Generating noise track of same length as original video ..." $C_INFO
+NOISE="noise.mp3"
 $FFMPEG -f lavfi -i nullsrc=s=1280x720 -filter_complex "geq=random(1)*255:128:128;aevalsrc=-2+random(0)" -t $LENGTH $TMP$NOISE
 $FFMPEG -i $TMP$NOISE -map 0 -map -v ${TMP}void.$NOISE
+
 $LOG "Quieting down noise track ..." $C_INFO
 $FFMPEG -i ${TMP}void.$NOISE -filter:a "volume=0.2" ${TMP}quiet.$NOISE
+
 $LOG "Merging original audio and noise track into final audio track ..." $C_INFO
 $FFMPEG -i ${TMP}input-audio.mp3 -i ${TMP}quiet.$NOISE -filter_complex amix=inputs=2:duration=shortest ${TMP}output-audio.mp3
+
 $LOG "Re-encoding modified frames into video ..." $C_INFO
-$FFMPEG -framerate $FPS -pattern_type glob -i 'tmp/*.jpg' \
+$FFMPEG -framerate $FPS -pattern_type glob -i "$TMP*.jpg" \
   -c:v libx264 -pix_fmt yuv420p silent.out.mp4
+
 $LOG "Re-encoding final audio track into video ..." $C_INFO
 $FFMPEG -i silent.out.mp4 -i ${TMP}output-audio.mp3 -c copy -map 0:v:0 -map 1:a:0 out.mp4
+
 $LOG "Cleaning up ..." $C_INFO
 rm -rf $TMP silent.out.mp4
